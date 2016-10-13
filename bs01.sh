4ba5900f75c19f7225b9d7cce36f4e265a5b8864
sudozgj <13714515160@163.com>
2016-10-13 18:02:33 +0800

    Add files via upload
    do what you want to do on linux machine,and control by java

---
+++ bs01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "hello,this is the fisrt line in bash!"
+echo "no 2 line"
+echo "no 3"
+echo "the last line of the file"
+
+#ps -ef | grep zabbix
+
+while true
+do
+	echo `date +%F" "%H:%M:%S`
+#	java -jar doorHandler.jar /home/shc/DZG/IPC_Camera/doorLog
+	sleep 2
+done
